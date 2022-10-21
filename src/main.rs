1be47d3855f826138434fd9d5d90d4c8990a4c00
ptakpatryk <ptaszqq@gmail.com>
2022-10-21 23:24:14 +0100

    first itegration test

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,9 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use std::net::TcpListener;
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("Word");
-
-    format!("Hello {}", &name)
-}
-
-async fn healthy_check_handler() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/healthy-check", web::get().to(healthy_check_handler))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind the port");
+    run(listener)?.await
 }
