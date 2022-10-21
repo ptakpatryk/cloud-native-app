6e43b1b87c20af1549e712f1b2f254f5a3a75ae7
ptakpatryk <ptaszqq@gmail.com>
2022-10-21 17:07:49 +0100

    health_check

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
-fn main() {
-    let text = 'h';
-    println!("Hello, world! {}", text);
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("Word");
+
+    format!("Hello {}", &name)
+}
+
+async fn healthy_check_handler() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/healthy-check", web::get().to(healthy_check_handler))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
