9a8309fa05b412ed11d5ab0f045ca536a366580c
ptakpatryk <ptaszqq@gmail.com>
2022-10-26 19:06:35 +0100

    logging

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,14 @@ use std::net::TcpListener;
 use sqlx::PgPool;
 use zero2prod::configuration;
 use zero2prod::startup;
+use zero2prod::telemetry::get_subscriber;
+use zero2prod::telemetry::init_subscriber;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // Panic if can't read config file
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let configuration = configuration::get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,16 +1,30 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize, Debug)]
-pub struct SubsribeForm {
+pub struct SubscribeForm {
     name: String,
     email: String,
 }
 
 // #[post("/subscribe")]
-pub async fn subscribe(form: web::Form<SubsribeForm>, pool: web::Data<PgPool>) -> HttpResponse {
+pub async fn subscribe(form: web::Form<SubscribeForm>, pool: web::Data<PgPool>) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    );
+
+    let _request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
+
     match sqlx::query!(
         r#"INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)"#,
         Uuid::new_v4(),
@@ -19,11 +33,18 @@ pub async fn subscribe(form: web::Form<SubsribeForm>, pool: web::Data<PgPool>) -
         Utc::now()
     )
     .execute(pool.get_ref())
+    .instrument(query_span)
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            tracing::info!(
+                "Request {}: New subscriber details have been saved",
+                request_id
+            );
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            tracing::error!("Request {}: Failed to execute query: {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
